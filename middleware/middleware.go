4d4adac2313fd75ccf7ac620049e25816c9ecd49
Skalefou <skamsagar@gmail.com>
2024-05-09 01:27:30 +0200

    Add token verification middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,8 +3,13 @@
 package middleware
 
 import (
+	"PCS-API/models"
+	"PCS-API/utils"
+	"errors"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"time"
 )
 
@@ -18,4 +23,33 @@ func CORS(router *gin.Engine) {
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
+}
+
+// AuthMiddleware Vérifie le token de connexion pour accèder à certaines parties de l'API
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		claims := &models.Claims{}
+
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return utils.TokenKey, nil
+		})
+
+		if err != nil {
+			if errors.Is(err, jwt.ErrSignatureInvalid) {
+				c.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		if !token.Valid {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		c.Set("idUser", claims.IdUser)
+		c.Next()
+	}
 }
