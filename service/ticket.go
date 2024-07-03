1f07c435a45fd32546cef77b991fe2ec44ed1163
Skalefou <skamsagar@gmail.com>
2024-07-03 15:53:53 +0200

    fix

--- service/ticket.go
+++ service/ticket.go
@@ -24,21 +24,18 @@ func TicketGetAll(c *gin.Context) {
 func TicketUpdateById(c *gin.Context) {
 	var err error
 
-	// Parse json from the body to a ticket struct
 	var ticket models.Ticket
 	if err = c.BindJSON(&ticket); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	// Update the ticket in the database
 	ticket, err = repository.TicketUpdateById(ticket)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 		return
 	}
 
-	// Return the updated ticket
 	c.JSON(http.StatusOK, gin.H{"ticket": ticket})
 }
 
